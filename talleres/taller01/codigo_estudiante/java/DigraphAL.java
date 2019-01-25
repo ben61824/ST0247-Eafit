ca429de32b7b062dc78cd5d63b633058e59783b5
ben61824 <37964425+ben61824@users.noreply.github.com>
2019-01-25 13:54:46 -0500

    Update DigraphAL.java

--- talleres/taller01/codigo_estudiante/java/DigraphAL.java
+++ talleres/taller01/codigo_estudiante/java/DigraphAL.java
@@ -1,13 +1,15 @@
-import java.util.ArrayList;
 
 /**
  * Implementacion de un grafo dirigido usando listas de adyacencia
  *
- * @author Mauricio Toro, Mateo Agudelo, <su nombre>
+ * @author Mauricio Toro, Mateo Agudelo, Ricardo Saldarriaga, Benjamin de la Torre
  */
+import java.util.ArrayList;
+import java.util.LinkedList;
+import javafx.util.Pair;
 public class DigraphAL extends Digraph {
 	
-
+ LinkedList<LinkedList<Pair<Integer,Integer>>> lista; 
 	/**
 	* Constructor para el grafo dirigido
 	* @param vertices el numero de vertices que tendra el grafo dirigido
@@ -15,7 +17,9 @@ public class DigraphAL extends Digraph {
 	*/
 	public DigraphAL(int size) {
 		super(size);
-		
+		lista = new LinkedList();
+    		for (int i = 1; i <= size; i++)
+       			lista.add(new LinkedList<Pair<Integer,Integer>>());
 	}
 
 	/**
@@ -26,7 +30,8 @@ public class DigraphAL extends Digraph {
 	* @param weight el peso de la longitud entre source y destination
 	*/
 	public void addArc(int source, int destination, int weight) {
-		
+		LinkedList<Pair<Integer,Integer>> listaSource = lista.get(source);
+		listaSource.add(new Pair(destination,weight));
 	}
 
 	/**
@@ -38,7 +43,12 @@ public class DigraphAL extends Digraph {
  	* @see <a href="https://docs.oracle.com/javase/8/docs/api/java/util/ArrayList.html"> Ver documentacion ArrayList </a>
 	*/
 	public ArrayList<Integer> getSuccessors(int vertex) {
-		
+		 // Una nueva lista que saque los sucesores
+     		ArrayList<Integer> respuesta = new ArrayList();
+     		LinkedList<Pair<Integer,Integer>> listaDeParejas = lista.get(vertex);
+     		for (Pair<Integer,Integer> pareja: listaDeParejas)
+       			respuesta.add(pareja.getKey()); //segunda parte de la pareja
+     		return respuesta;
 	}
 
 	/**
@@ -49,7 +59,15 @@ public class DigraphAL extends Digraph {
 	* @return un entero con dicho peso
 	*/	
 	public int getWeight(int source, int destination) {
-		
+	    // Un ciclo que dentro de la lista source, cuando
+	    //encuentre que sea el destino correcto, retorne el peso
+	    int weight = 0;
+	    LinkedList<Pair<Integer,Integer>> listaDeParejas = lista.get(source);
+	    for (Pair<Integer,Integer> pareja: listaDeParejas)
+	    	if (pareja.getKey()==destination)
+			weight = pareja.getValue();
+	    return weight;
+
 	}
 
 }
