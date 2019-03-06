50ab0ad402a2d5b2f39403be77dcedded054b079
ben61824 <37964425+ben61824@users.noreply.github.com>
2019-03-05 19:24:23 -0500

    Update Taller6.java

--- talleres/taller06/codigo_estudiante/Taller6.java
+++ talleres/taller06/codigo_estudiante/Taller6.java
@@ -6,18 +6,41 @@
  */
 public class Taller6 {
 
-	/**
-	* Metodo que dado un entero n y un conjunto de denominciones de dinero
-	* busque la manera optima de dar el cambio
-	* @param n cantidad a devolver
-	* @param denominaciones conjunto de denominaciones de dinero (monedas, billetes)
+	/*Metodo que En el cual el usuario ingresa el monto de dinero a cambiar*/
+	static String cambiost="";
+    public static void ValorMonto(){
+        int monto=0;
+        int cambio=0;
+        String cambiost="";
+        Scanner teclado = new Scanner(System.in);
+        System.out.println(" Digite monto a cambiar: ");
+        monto = teclado.nextInt();
+        RealizarCambio(monto,cambio);    
+    }
+    /*Metodo que cambia vorazmente el dinero del usuario con monedas*/
+    public static void RealizarCambio(double monto,double cambio){    
+        int[] monedas = {100,200,500,1000};
+        while(cambio!=monto){
+        int i=monedas.length-1;
+        System.out.println("Para cambiar: " + monto + " Pesos, se necesita");
+           while(i>=0){
+            if((cambio+monedas[i])<=monto){
+             cambio = cambio+monedas[i];
+             cambiost = "\n Una moneda de : "+ monedas[i];
+             System.out.println(cambiost);
+            }
+            else{
+                i=i-1;
+            }       
+        }  
+       }      
+    }
+    
+    public static void main(String[] args){
+        ValorMonto(); 
+    }
 
-	* @return un conjunto de unidades por denominacion
-	*/
-	public static int[] cambioGreedy(int n, int[] denominaciones) {
-		
-	}
-
+//----------------------------------------------------------------------------------------------------------------
 	/**
 	* Metodo que recorre todo el grafo con la intencion de buscar un
 	* camino que represente el menor costo pasando por todos los vertices exactamente
